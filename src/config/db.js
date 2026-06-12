daadcc1b2ca721b45ebddcc4885fa3d827c8209f
NICOGATT <nicogatt1@hotmail.com>
2026-06-12 08:44:28 -0300

    Inicilizamos el proyecto, puse el docker compose para la imagen de mongodb, instale dependencias, y ejecute docker compose up para ver si funcionaba todo bien

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose'); 
+
+const conectarDb = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Conexion exitosa con mongodb")
+    } catch (error) {
+        console.error("Error al conectar con a MongoDB", error.message);
+    }
+}
+
+module.exports = conectarDb;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 console.log("UnaHur - Anti-Social net");
+
+const express = require("express");
+const dotenv = require("dotenv");
+const conectarDb = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+conectarDb();
+
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en puerto ${port}`);
+});
