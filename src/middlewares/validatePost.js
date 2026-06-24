9c789243ebbbed4a13c1fd6e08e674cc28dbda37
ErrEgEdEcE <rocio.garciadelcorro@unahur.edu.ar>
2026-06-24 10:40:08 -0300

    verifica si existe usuario

--- src/middlewares/validatePost.js
+++ src/middlewares/validatePost.js
@@ -1,10 +1,22 @@
 const Post = require('../models/post.model');
+const Usuario = require('../models/Usuario');
 
 const validarBodyPost = async(req, res, next) => {
     try{
         if (req.body.userId) {
             req.body.user = req.body.userId;
         }
+
+        if (req.body.user) {
+            const existeUsuario = await Usuario.findById(req.body.user);
+            if (!existeUsuario) {
+                return res.status(404).json({
+                    message: "Error de validación",
+                    errors: ["El usuario no existe en la base de datos."]
+                });
+            }
+        }
+        
         const postTemporal = new Post(req.body);
         await postTemporal.validate();
         next();
